6a37f797d853b05db7ec4cd1c5eabf450f7eed0d
YoProgrammer <yuttasakcom@gmail.com>
2017-07-09 20:43:09 +0700

    ทำความเข้าใจเรื่อง struct reciver

--- 5.struct/main.go
+++ 5.struct/main.go
@@ -7,29 +7,29 @@ type person struct {
 	NickName string
 }
 
-func main() {
-	// p1 := person{
-	// 	Name:     "TT",
-	// 	NickName: "YoProgrammer",
-	// }
-	var p1 person
-	p1.Name = "TT"
-	p1.NickName = "YoProgrammer"
-	fmt.Println(p1)
-
-	// mutatePerson(p1)
-	mutatePerson(&p1)
-	p1.mutate()
-	fmt.Println(p1)
-}
-
-//func mutatePerson(p person) {
-func mutatePerson(p *person) {
-	p.Name = "Hacker"
-	fmt.Println("inside mutate:", p)
-}
-
 func (p *person) mutate() {
 	p.Name = "Hacker"
-	fmt.Println("inside mutate method:", p)
+	fmt.Println("In", *p)
+}
+
+func main() {
+	p1 := person{
+		Name:     "TT",
+		NickName: "YoProgrammer",
+	}
+	fmt.Println(p1)
+
+	mutatePerson(p1)
+	fmt.Println("Out", p1)
+
+	p1.mutate()
+	fmt.Println("Out", p1)
+
+	mutatePerson(p1)
+	fmt.Println(p1)
+}
+
+func mutatePerson(p person) {
+	p.Name = "Normal"
+	fmt.Println("In:", p)
 }
